36eeb093f9ec0049c58dcedd59f5a1a920e60a24
Alejandro St <animatk@gmail.com>
2018-03-24 12:30:24 -0500

    Initial Commit

---
+++ ExpandibleCollectionViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandibleCollectionViewCell
+//
+//  Created by Alejandro St on 24/03/18.
+//  Copyright © 2018 Alejandro St. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
