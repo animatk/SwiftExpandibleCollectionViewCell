b6d5dcc33632ce322b8ac0c0a924bc03ef2a1441
Alejandro St <animatk@gmail.com>
2018-03-24 20:02:29 -0500

    agregar elementos (items) del tipo Places a la CollectionView agregar en el evento de seleccionar un row una serie de animaciones para desplegar la celda seleccionada estilizar celda con boton de cerrar (por defecto oculto) textos y acciones.

--- ExpandibleCollectionViewCell/Resources/PlacesCollectionView.swift
+++ ExpandibleCollectionViewCell/Resources/PlacesCollectionView.swift
@@ -10,19 +10,61 @@ import UIKit
 
 class PlacesCollectionView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
-    let items = [Places]()
+    var items = [Places]()
 	let cellId = "cell"
-	
+
 	let collectionView : UICollectionView = {
 		let menu = UICollectionViewFlowLayout()
 		let cv = UICollectionView(frame: .zero, collectionViewLayout: menu)
 		cv.backgroundColor = UIColor.rgb(200, green: 230, blue: 255)
-		cv.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		cv.contentInset = UIEdgeInsets(top: -20, left: 0, bottom: 0, right: 0)
 		return cv
 	}()
 	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+		
+		let place1: Places = {
+			let pl = Places()
+			pl.image = "IMG_001"
+			pl.name = "Sesame snaps donut pudding sugar"
+			pl.desc = "Cupcake dragée ice cream topping lollipop macaroon. Liquorice chocolate cake gummies topping ice cream lollipop sugar plum. Cotton candy danish halvah gummi bears chocolate cake sugar plum croissant cheesecake. "
+			return pl
+		}()
+		let place2: Places = {
+			let pl = Places()
+			pl.image = "IMG_002"
+			pl.name = "Liquorice chocolate cake"
+			pl.desc = "Cupcake dragée ice cream topping lollipop macaroon. Liquorice chocolate cake gummies topping ice cream lollipop sugar plum. Cotton candy danish halvah gummi bears chocolate cake sugar plum croissant cheesecake. "
+			return pl
+		}()
+		let place3: Places = {
+			let pl = Places()
+			pl.image = "IMG_003"
+			pl.name = "Gummies topping ice cream"
+			pl.desc = "Cupcake dragée ice cream topping lollipop macaroon. Liquorice chocolate cake gummies topping ice cream lollipop sugar plum. Cotton candy danish halvah gummi bears chocolate cake sugar plum croissant cheesecake. "
+			return pl
+		}()
+		let place4: Places = {
+			let pl = Places()
+			pl.image = "IMG_004"
+			pl.name = "Cupcake dragée ice cream"
+			pl.desc = "Cupcake dragée ice cream topping lollipop macaroon. Liquorice chocolate cake gummies topping ice cream lollipop sugar plum. Cotton candy danish halvah gummi bears chocolate cake sugar plum croissant cheesecake. "
+			return pl
+		}()
+		let place5: Places = {
+			let pl = Places()
+			pl.image = "IMG_005"
+			pl.name = "Topping lollipop macaroon."
+			pl.desc = "Cupcake dragée ice cream topping lollipop macaroon. Liquorice chocolate cake gummies topping ice cream lollipop sugar plum. Cotton candy danish halvah gummi bears chocolate cake sugar plum croissant cheesecake. "
+			return pl
+		}()
+		
+		items.append(place1)
+		items.append(place2)
+		items.append(place3)
+		items.append(place4)
+		items.append(place5)
 		
 		//setup collection view
 		collectionView.dataSource = self
@@ -45,15 +87,83 @@ class PlacesCollectionView: UIView, UICollectionViewDataSource, UICollectionView
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 5
+		return items.count
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PlacesCell
+		
+		let item = items[indexPath.item]
+		
+		cell.closeBtn.isHidden = true
+		cell.placeTitle.text = item.name
+		cell.placeDesc.text = item.desc
+		cell.placeContent.isHidden = true
+		cell.placeImage.image = UIImage(named: item.image!)
+		
 		return cell
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 		return CGSize(width: collectionView.frame.width, height: 200)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return 16
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let cell = collectionView.cellForItem(at: indexPath) as! PlacesCell
+		
+		cell.superview?.bringSubview(toFront: cell)
+		
+		cell.placeContent.alpha = 0;
+		cell.placeContent.isHidden = false
+		
+		
+		UIView.animate(
+			withDuration: 0.8
+			, delay: 0
+			, usingSpringWithDamping: 50
+			, initialSpringVelocity: 1
+			, options: .curveEaseInOut
+			, animations: {
+				
+					collectionView.isScrollEnabled = false
+					cell.placeImage.frame = CGRect(x: 0, y: 0, width: collectionView.frame.width, height: 300)
+					cell.frame =   collectionView.bounds
+				
+				
+			}) {_ in
+				
+				cell.closeBtn.isHidden = false
+				cell.closeBtn.tag = indexPath.item
+				cell.closeBtn.addTarget(self, action: #selector(self.closePlaceDetails), for: .touchUpInside)
+				
+				cell.placeDesc.sizeToFit()
+				
+				UIView.animate(
+					withDuration: 1
+					, delay: 0
+					, usingSpringWithDamping: 1
+					, initialSpringVelocity: 1
+					, options: .curveEaseInOut
+					, animations: {
+				
+						cell.placeContent.alpha = 1;
+					})
+			}
+	}
+	
+	@objc func closePlaceDetails(sender: UIButton)
+	{
+		
+		
+		let indexPath = NSIndexPath(row: sender.tag, section: 0) as IndexPath
+		collectionView.isScrollEnabled = true
+		collectionView.reloadItems(at: [indexPath])
+		
+		
+		
 	}
 }
--- ExpandibleCollectionViewCell/Views/PlacesCell.swift
+++ ExpandibleCollectionViewCell/Views/PlacesCell.swift
@@ -10,6 +10,71 @@ import UIKit
 
 class PlacesCell: UICollectionViewCell {
 	
+	let closeBtn : UIButton = {
+		let bt = UIButton()
+		bt.setTitle("X", for: .normal)
+		bt.titleLabel?.font = UIFont(name: "Helvetica", size: 16)
+		bt.backgroundColor = UIColor.rgb(100, green: 100, blue: 100, alpha: 0.5)
+		bt.layer.cornerRadius = 13
+		bt.isHidden = true
+		return bt
+	}()
+	
+	let placeImage: UIImageView = {
+        let im = UIImageView()
+        im.contentMode = .scaleAspectFill
+		im.backgroundColor = .lightGray
+		im.layer.masksToBounds = true
+		return im
+    }()
+	
+    let placeContent : UIView = {
+		let vi = UIView()
+		vi.backgroundColor = .white
+    	return vi
+	}()
+	
+	let placeTitle: UILabel = {
+		let lb = UILabel()
+		lb.text = "Place name"
+		lb.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+		return lb
+	}()
+	
+	let placeDesc: UILabel = {
+		let lb = UILabel()
+		lb.text = "Description of place"
+		lb.font = UIFont(name: "HelveticaNeue-Reguar", size: 16)
+		lb.textColor = .gray
+		lb.numberOfLines = 0
+		lb.lineBreakMode = .byWordWrapping
+		//lb.sizeToFit()
+		return lb
+	}()
+	
+	let optionsView = UIView()
+	
+	let option1 : UIButton = {
+		let bt = UIButton()
+		bt.setTitle("Reserve now", for: .normal)
+		bt.titleLabel?.font = UIFont(name: "Helvetica", size: 16)
+		bt.layer.cornerRadius = 3
+		bt.backgroundColor = UIColor.rgb(204, green: 102, blue: 255)
+		bt.setTitleColor(.white, for: .normal)
+		return bt
+	}()
+	
+	let option2 : UIButton = {
+		let bt = UIButton()
+		bt.setTitle("Create reminder", for: .normal)
+		bt.titleLabel?.font = UIFont(name: "Helvetica", size: 16)
+		bt.layer.cornerRadius = 4
+		bt.backgroundColor = UIColor.rgb(217, green: 217, blue: 217)
+		bt.setTitleColor(.black, for: .normal)
+		return bt
+	}()
+	
+	
     override init(frame: CGRect) {
 		super.init(frame: frame)
 		
@@ -20,27 +85,42 @@ class PlacesCell: UICollectionViewCell {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
-	let placeImage: UIImageView = {
-        let im = UIImageView()
-        im.contentMode = .scaleAspectFit
-		im.backgroundColor = .lightGray
-        return im
-    }()
-	
-    let placeContent : UIView = {
-		let vi = UIView()
-		vi.backgroundColor = .white
-    	return vi
-	}()
-	
     func setupView() {
 		
-		addSubview(placeImage)
-		addSubview(placeContent)
+		backgroundColor  = .white
 		
-		addConstraintsWithFormat("H:|-[v0]-|", views: placeImage)
-		addConstraintsWithFormat("H:|-[v0]-|", views: placeContent)
-		addConstraintsWithFormat("V:|-[v0(150)][v1]-|", views: placeImage, placeContent)
+		addSubview(placeImage)
+		addSubview(placeTitle)
+		addSubview(placeContent)
+		addSubview(closeBtn)
+		
+		addConstraintsWithFormat("H:|-20-[v0(25)]", views: closeBtn)
+		addConstraintsWithFormat("V:|-20-[v0(25)]", views: closeBtn)
+		
+		addConstraintsWithFormat("H:|[v0]|", views: placeImage)
+		addConstraintsWithFormat("H:|-16-[v0]-16-|", views: placeTitle)
+		addConstraintsWithFormat("H:|-16-[v0]-16-|", views: placeContent)
+		addConstraintsWithFormat("V:|[v0(>=150,<=300)]-10-[v1(>=20,<=30)]-16-[v2]|", views: placeImage, placeTitle, placeContent)
+		
+		
+		placeContent.addSubview( placeDesc )
+		placeContent.addSubview( optionsView )
+		
+		placeContent.addConstraintsWithFormat("H:|-[v0]-|", views: placeDesc)
+		placeContent.addConstraintsWithFormat("H:|-[v0]-|", views: optionsView)
+		placeContent.addConstraintsWithFormat("V:|[v0]-16-[v1]-|", views: placeDesc, optionsView)
+		
+		optionsView.addSubview(option1)
+		optionsView.addSubview(option2)
+		
+		
+		optionsView.addConstraintsWithFormat("H:|-[v0]-16-[v1(v0)]-|", views: option1, option2)
+		optionsView.addConstraintsWithFormat("V:|-[v0(40)]-|", views: option1)
+		optionsView.addConstraintsWithFormat("V:|-[v0(40)]-|", views: option2)
+		
+		
+		
+		
 		
 	}
 }
